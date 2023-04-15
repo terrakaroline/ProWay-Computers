1d763c59bfc63dda69594266c018bc07dd7570bd
Karoline <shiamosan@gmail.com>
2023-04-15 18:00:21 -0300

    funcionando template de pg de alunos

--- proway-computers/src/app/produtos/detalhes-produto/detalhes-produto.component.ts
+++ proway-computers/src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProdutosService } from 'src/app/produtos.service';
+import { IProduto } from '../produtos';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalhes-produto',
   templateUrl: './detalhes-produto.component.html',
   styleUrls: ['./detalhes-produto.component.css']
 })
-export class DetalhesProdutoComponent {
+export class DetalhesProdutoComponent implements OnInit {
 
+  produto: IProduto | undefined;
+
+  quantidade = 1;
+
+  constructor(
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId);
+  }
 }
