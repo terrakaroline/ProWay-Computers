d274795bc3ae2f8bfbacff9fac599e96c7de89d5
Karoline <shiamosan@gmail.com>
2023-04-15 14:54:58 -0300

    pagina nao encontrada rota

--- proway-computers/src/app/app-routing.module.ts
+++ proway-computers/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
-const routes: Routes = [{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule)},
-{path: "", redirectTo: "produtos", pathMatch: "full"}
+const routes: Routes = [
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule)},
+  {path: "", redirectTo: "produtos", pathMatch: "full"},
+  {path: "**", component: NaoEncontradoComponent}
 ];
 
 @NgModule({
